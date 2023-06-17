51cf06d7aa3566ea8be2839a64452ff2b2997cda
Caesar Ngyn <caesarngyn@gmail.com>
2023-06-17 16:29:22 +0700

    separate layout components

---
+++ src/screens/Portfolio.tsx
@@ -0,0 +1,44 @@
+import LeftPart from "../components/LeftPart"
+import PreLoader from "../components/PreLoader"
+import RightPart from "../components/RightPart"
+import Mobile from "../components/mobile/Mobile"
+
+const Portfolio = () => {
+  return (
+    <>
+      <div className="arlo_tm_wrapper_all">
+
+        <div id="arlo_tm_popup_blog">
+          <div className="container">
+            <div className="inner_popup scrollable"></div>
+          </div>
+          <span className="close"><a href="#"></a></span>
+        </div>
+
+
+
+        <PreLoader />
+
+
+
+
+        <Mobile />
+
+
+        <div className="arlo_tm_content">
+
+
+          <LeftPart />
+
+
+          <RightPart />
+
+          <a className="arlo_tm_totop" href="#"></a>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Portfolio
